f0050c8decd85218351275d01dea36cf10b49d0d
Alexander Goldberg <algold@gmail.com>
2013-08-22 20:19:37 +0300

    Initial commit

---
+++ info.el
@@ -0,0 +1,3 @@
+(live-pack-name "ido-power-pack")
+(live-pack-version "0.2.0-SANPSHOT")
+(live-pack-description "Ido hacks and improvements")
---
+++ init.el
@@ -0,0 +1,5 @@
+;; Always reset the theme before the change - fixes a lot of small problems
+(install-packs '(flx-ido))
+
+(require 'flx-ido)
+(flx-ido-mode 1)')
