bff8c95cadeb9bdcf4f0309a8b40bf8310fac694
Alexander Goldberg <algold@gmail.com>
2013-08-22 22:53:17 +0300

    * init.el: Add iflipb to cycle buffers

--- init.el
+++ init.el
@@ -1,5 +1,12 @@
 ;; Always reset the theme before the change - fixes a lot of small problems
-(install-packs '(flx-ido))
+(install-packs '(flx-ido
+                 iflipb))
 
 (require 'flx-ido)
 (flx-ido-mode 1)
+
+;; Ignore .DS_Store files with ido mode
+(add-to-list 'ido-ignore-files "\\.DS_Store")
+
+(global-set-key [C-tab] 'iflipb-next-buffer)
+(global-set-key [C-S-tab] 'iflipb-previous-buffer-buffer)
